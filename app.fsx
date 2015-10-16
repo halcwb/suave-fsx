15c2db735a1adc750b0be492b08428c4ae9ce518
Casper Bollen <halcwb@gmail.com>
2015-10-16 19:04:27 +0200

    Changed heroku setup

--- app.fsx
+++ app.fsx
@@ -1,9 +1,3 @@
-#if BOOTSTRAP
-System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/0.27.2/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
-#r "paket.exe"
-Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
-#endif
 //---------------------------------------------------------------------
 #I "packages/Suave/lib/net40"
 #r "packages/Suave/lib/net40/Suave.dll"
